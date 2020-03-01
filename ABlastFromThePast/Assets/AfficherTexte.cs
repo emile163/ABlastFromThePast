99370f76f43de7c3ef23f53b6b443437de9a3f13
Bob-Ginnos <60973189+Bob-Ginnos@users.noreply.github.com>
2020-03-01 04:59:00 -0500

    0

--- ABlastFromThePast/Assets/AfficherTexte.cs
+++ ABlastFromThePast/Assets/AfficherTexte.cs
@@ -11,18 +11,28 @@ public class AfficherTexte : MonoBehaviour
     }
  public GameObject Target;
   public GameObject TextBox;
-    public Vector3 distancePNJ_P;   
-   
+    public GameObject Text;
+    public Vector3 distancePNJ_P;
+    bool isActive = false;
     // Update is called once per frame
     void Update()
     {
         distancePNJ_P = Target.transform.position- this.transform.position;
-        if (distancePNJ_P.magnitude< 1)
+        if (distancePNJ_P.magnitude < 1)
         {
-            (TextBox).SetActive(true) ;
+            Text.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                isActive = !isActive;
+                TextBox.SetActive(isActive);
+                
+                // (TextBox).SetActive(true) ;
+            }
         }
         else
         {
+            Text.SetActive(false);
+
             (TextBox).SetActive(false);
         }
     }
