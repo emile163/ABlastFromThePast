4354eb4f6171e5ed6640bc15d858217f6ee514f7
Bob <sspsychox@gmail.com>
2020-03-04 12:08:54 -0500

    Revert "PNJ et dialogue"
    This reverts commit d3b6371b5748aee7acff58dced6cdfe6c9c2b809.

--- ABlastFromThePast/Assets/AfficherTexte.cs
+++ ABlastFromThePast/Assets/AfficherTexte.cs
@@ -10,7 +10,6 @@ public class AfficherTexte : MonoBehaviour
         
     }
  public GameObject Target;
-<<<<<<< Updated upstream
   public GameObject TextBox;
     public Vector3 distancePNJ_P;   
    
@@ -26,34 +25,6 @@ public class AfficherTexte : MonoBehaviour
         {
             (TextBox).SetActive(false);
         }
-=======
-    public DialogueTrigger
-        dialogueTrigger;
-    private bool isInside = false;
-  //public GameObject TextBox;
-    public Vector3 distancePNJ_P;
-
-    // Update is called once per frame
-    void Update()
-    {
-        distancePNJ_P = Target.transform.position - this.transform.position;
-        if (Input.GetKeyDown(KeyCode.E)){
-            if (distancePNJ_P.magnitude < 1 && !isInside)
-            {
-                
-                dialogueTrigger.TriggerDialogue();
-                isInside = true;
-            }
-            
-        }else if  (distancePNJ_P.magnitude > 1)
-            {
-                isInside = false;
-
-            FindObjectOfType<DialogueManager>().EndDialogue() ;
-            
-            }
-      //  else isInside = false; 
->>>>>>> Stashed changes
     }
 
 
