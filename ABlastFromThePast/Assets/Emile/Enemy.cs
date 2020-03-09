c06bce9914642f96d50cca831ef39857cece20f9
emile163 <bergeronemile4@gmail.com>
2020-03-09 17:49:53 -0400

    Combat

---
+++ ABlastFromThePast/Assets/Emile/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int maxHealth = 100;
+    int currentHealth;
+    
+    void Start()
+    {
+        currentHealth = maxHealth;
+        
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0) {
+            Die();
+        }
+    
+    }
+    void Die()
+    {
+
+        this.enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+    }
+}
---
+++ ABlastFromThePast/Assets/Emile/PlayerCombat.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+   
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 40;
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+    void Update()
+    {
+        if(Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+    }
+    }
+    void Attack()
+    {
+      Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        
+        }
+    
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
