72990e52fd5b663cabed9778a21ec2dd346adf4c
emile163 <bergeronemile4@gmail.com>
2020-03-11 12:07:02 -0400

    ctuyklrf

--- ABlastFromThePast/Assets/Emile/Enemy.cs
+++ ABlastFromThePast/Assets/Emile/Enemy.cs
@@ -7,7 +7,9 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     public int maxHealth = 100;
     int currentHealth;
-    
+    public GameObject host;
+    public GameObject FXDeathPrefab;
+    public GameObject RandomDrop;
     void Start()
     {
         currentHealth = maxHealth;
@@ -15,6 +17,12 @@ public class Enemy : MonoBehaviour
     }
     public void TakeDamage(int damage)
     {
+        if (FXDeathPrefab)
+        {
+            GameObject fxDeath = Instantiate(FXDeathPrefab, transform.position, FXDeathPrefab.transform.rotation) as GameObject;
+            Destroy(fxDeath, 3f);
+        }
+
         currentHealth -= damage;
 
         if (currentHealth <= 0) {
@@ -24,8 +32,13 @@ public class Enemy : MonoBehaviour
     }
     void Die()
     {
-
+        Destroy(host);
         this.enabled = false;
         GetComponent<Collider2D>().enabled = false;
+
+        if (RandomDrop)
+        {
+            Instantiate(RandomDrop, transform.position, transform.rotation);
+        }
     }
 }
