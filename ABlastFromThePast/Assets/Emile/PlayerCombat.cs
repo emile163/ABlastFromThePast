d7c305e4e9d4a7b7249e1c63c80b6f63c7b81c3c
emile163 <bergeronemile4@gmail.com>
2020-03-31 12:14:34 -0400

    attack

---
+++ ABlastFromThePast/Assets/Emile/PlayerCombat.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+   
+    public Transform attackPoint;
+    
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 40;
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+    public Transform pos;
+    bool movingRight ;
+    bool movingLeft;
+    bool movingUp ;
+    bool movingDown;
+    bool moving;
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+    }
+    void Update()
+    {
+        animator.SetBool("attack", false);
+
+        if (moving == true)
+        {
+            if (Input.GetKey(KeyCode.A) && !(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+            {
+                movingUp = false;
+                movingDown = false;
+            }
+            if (Input.GetKey(KeyCode.D) && !(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+            {
+                movingUp = false;
+                movingDown = false;
+            }
+            if (Input.GetKey(KeyCode.W) && !(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)))
+            {
+
+                movingRight = false;
+                movingLeft = false;
+            }
+            if (Input.GetKey(KeyCode.S) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+            {
+                movingRight = false;
+                movingLeft = false;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moving = true;
+            movingUp = true;
+            movingDown = false;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moving = true;
+            movingUp = false;
+            movingDown = true;
+        }
+        
+         
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            moving = true;
+            movingLeft = true;
+            movingRight = false;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moving = true;
+            movingLeft = false;
+            movingRight = true;
+        }
+        if(!(Input.GetKey(KeyCode.D)|| Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.S)))
+        {
+            
+            moving = false;
+        }
+
+
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+               
+
+                    attackPoint.position = pos.position;
+
+                 
+                
+
+                if (movingUp == true)
+                {
+                    attackPoint.transform.position += new Vector3(0, 0.18f, 0);
+                    animator.SetBool("attack", true);
+                }
+                if (movingDown == true)
+                {
+                    attackPoint.transform.position += new Vector3(0, -0.18f, 0);
+                    animator.SetBool("attack", true);
+                }
+               if (movingLeft == true)
+                {
+                    attackPoint.transform.position += new Vector3(-0.18f, 0, 0);
+                    animator.SetBool("attack", true);
+                }
+               if (movingRight == true)
+                {
+                    attackPoint.transform.position += new Vector3(0.18f, 0, 0);
+                    animator.SetBool("attack", true);
+                }
+               
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+               
+            }
+    }
+    }
+    void Attack()
+    {
+        
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        
+        }
+        
+
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange); 
+        
+
+    }
+
+}
