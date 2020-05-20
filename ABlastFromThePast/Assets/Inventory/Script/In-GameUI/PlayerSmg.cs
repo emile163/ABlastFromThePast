38684f5bfdf289c124f9668988135f31163ead04
Hassaoui <clement@hassaoui.com>
2020-05-20 12:09:15 -0400

    settings

---
+++ ABlastFromThePast/Assets/Inventory/Script/In-GameUI/PlayerSmg.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSmg : MonoBehaviour
+{
+    public float MaxHealth = 100f;
+
+    public float armureMax = 100f;
+
+    private float currentHealth;
+
+    private float currentArmure;
+
+    public HealthBar hb;
+
+    public armurebar ab;
+
+    void Start()
+    {
+        currentArmure = armureMax;
+        currentHealth = MaxHealth;
+        hb.SetMaxHealth(MaxHealth);
+        ab.SetMaxArmure(armureMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+            takedmg(20);
+		}
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            takedmgArmure(20);
+        }
+    }
+
+    void takedmg (int dmg)
+	{
+        currentHealth -= dmg;
+
+        hb.SetHealth(currentHealth);
+
+    }
+
+    void takedmgArmure(int dmg)
+    {
+        currentArmure -= dmg;
+
+        ab.SetArmure(currentArmure);
+
+    }
+}
---
+++ ABlastFromThePast/Assets/Inventory/Script/In-GameUI/armurebar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class armurebar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxArmure(float Armure)
+	{
+		slider.maxValue = Armure;
+		slider.value = Armure;
+	}
+
+	public void SetArmure(float Armure)
+	{
+		slider.value = Armure;
+	}
+}
